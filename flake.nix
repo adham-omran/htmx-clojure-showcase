819ad65707e6766c2606d037d59054da763bd9b7
adham-omran <git@adham-omran.com>
2024-04-07 22:31:21 +0300

    Remove boot and leiningen from flake.nix

--- flake.nix
+++ flake.nix
@@ -10,9 +10,7 @@
       overlays = [
         (final: prev: rec {
           jdk = prev."jdk${toString javaVersion}";
-          boot = prev.boot.override { inherit jdk; };
           clojure = prev.clojure.override { inherit jdk; };
-          leiningen = prev.leiningen.override { inherit jdk; };
         })
       ];
       supportedSystems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
