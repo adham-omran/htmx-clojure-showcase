65a261231cd59dc2aeadbe2c6e3c1db315167201
adham-omran <git@adham-omran.com>
2024-04-07 22:38:20 +0300

    Update `showcase.views.index`

--- src/showcase/views/index.clj
+++ src/showcase/views/index.clj
@@ -16,41 +16,32 @@
                (page/include-css "./public/css/output.css") ;; tailwind output
                (page/include-js "./public/htmx.min.js") ;; lib
                (page/include-js "./public/js/highcharts.js")
-               (page/include-js "https://code.highcharts.com/modules/histogram-bellcurve.js")
-               (page/include-js "https://cdn.jsdelivr.net/npm/vega@5")
-               (page/include-js "https://cdn.jsdelivr.net/npm/vega-lite@5")
-               (page/include-js "https://cdn.jsdelivr.net/npm/vega-embed@6")
+               (page/include-js "./public/js/modules/histogram-bellcurve.js")
+               (page/include-js "./public/js/modules/accessibility.js")
+               (page/include-js "./public/js/vega5.js")
+               (page/include-js "./public/js/vega-lite5.js")
+               (page/include-js "./public/js/vega-embed6.js")
+               ;; Plotly
+               #_(page/include-js "./public/js/d3.js")
                [:body
                 [:h1
                  {:class "text-4xl flex w-full justify-center items-center p-4 mb-4 bg-slate-200"}
                  "Showcase"]
                 [:div
-                 {:class "grid grid-cols-2"}
+                 {:class "grid grid-flow-col auto-cols-auto gap-x-10"}
                  [:button
+                  {:class "bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full"
+                   :hx-post "/api/vega-lite"
+                   :hx-target "#showcase"}
                   "vega-lite"]
                  [:button
-                  {:class "bg-sky-300"}
-                  "highcharts"]]
-                #_[:div
-                   [:script
-                    (str (h/raw
-                          "vegaEmbed(document.currentScript.parentElement, "
-                          (core/generate-string
-                           {"data" {"values" [{"a" "A", "b" 39}]},
-                            "mark" "bar",
-                            "encoding"
-                            {"x" {"field" "a", "type" "nominal", "axis" {"labelAngle" 0}},
-                             "y" {"field" "b", "type" "quantitative"}}})
-                          ").catch(console.error);"))]]
-                [:div
-                 {:class "grid"}
-                 [:div#graph]
-                 [:div#showcase
-                  [:div
-                   [:script
-                    (str (h/raw (format "Highcharts.chart(document.currentScript.parentElement, %s)"
-                                        (plots/hc-bar-chart))))]]
-                  [:div
-                   [:script
-                    (str (h/raw (format "Highcharts.chart(document.currentScript.parentElement, %s)"
-                                        (plots/hc-historgram-chart))))]]]]]]))
+                  {:class "bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full"
+                   :hx-post "/api/highcharts"
+                   :hx-target "#showcase"}
+                  "highcharts"]
+                 [:button
+                  {:class "bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full"
+                   :hx-post "/api/plotly"
+                   :hx-target "#showcase"}
+                  "plotly"]]
+                [:div#showcase]]]))
