f64a555d90ac4f86f3561fb0dc4ebc772fdbf00e
adham-omran <git@adham-omran.com>
2024-04-07 19:20:04 +0300

    Add slider input

--- src/showcase/views/jupyter.clj
+++ src/showcase/views/jupyter.clj
@@ -16,19 +16,42 @@
 
                [:body
                 (page/include-js "./public/htmx.min.js")
+                (page/include-js "https://cdn.jsdelivr.net/npm/vega@5")
+                (page/include-js "https://cdn.jsdelivr.net/npm/vega-lite@5")
+                (page/include-js "https://cdn.jsdelivr.net/npm/vega-embed@6")
                 [:div
-                 [:textarea
-                  {:name "q"
-                   :hx-post "/api/input"
-                   :hx-trigger "keyup changed"
-                   :hx-target "#here"
-                   :hx-swap "innerHTML"}]
-                 [:div#here]]
-                [:div
-                 [:textarea
-                  {:name "q"
-                   :hx-post "/api/input"
-                   :hx-trigger "keyup changed"
-                   :hx-target "next"
-                   :hx-swap "innerHTML"}]
-                 [:div]]]]))
+                 [:div#vega]
+                 [:div
+                  [:input {:type "range"
+                           :id "v"
+                           :name "v"
+                           :min "0"
+                           :max "11"
+                           :hx-post "/api/slider-demo"
+                           :hx-trigger "change"
+                           :hx-swap "innerhtml"
+                           :hx-target "next"}
+                   "Input"]
+                  [:div]]
+                 [:div
+                  [:textarea
+                   {:name "q"
+                    :hx-post "/api/input"
+                    :hx-trigger "keyup changed"
+                    :hx-target "next"
+                    :hx-swap "innerHTML"}]
+                  [:div]]
+                 [:div
+                  [:textarea
+                   {:name "q"
+                    :hx-post "/api/input"
+                    :hx-trigger "keyup changed"
+                    :hx-target "next"
+                    :hx-swap "innerHTML"}]
+                  [:div]]
+                 [:div#next]]
+                [:button
+                 {:hx-get "/api/next-cell"
+                  :hx-target "#next"
+                  :hx-swap "outerHTML"}
+                 "Add cell."]]]))
